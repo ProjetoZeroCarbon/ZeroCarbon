aa45b07e635a3c1a02eaa7baefd4a8b1f067bb6c
eu <antonioaugusto1913@gmail.com>
2026-09-04 09:36:47 -0300

    .

--- LandingPage/index.js
+++ LandingPage/index.js
@@ -1,5 +1,11 @@
 const botao = document.getElementById('CriarConta');
 
-botao.addEventListener('click', () => {
-    console.log('clik');
+botao.addEventListener('mouseover', () => {
+    console.log('teste');
+    botao.style.backgroundColor = '#81da54';
+});
+
+botao.addEventListener('mouseout', () => {
+    botao.style.backgroundColor = ''
 })
+
